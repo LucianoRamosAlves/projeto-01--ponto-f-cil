a3719c011b0c7e8df72e605a6a7ead91e8d7cd16
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-08-20 06:53:14 -0300

    bronze: aula 02 - extension/init
    pip install flask-sqlalchemy
    
    pip show flask-sqlalchemy

---
+++ projeto_ponto_facil/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from app.extensions import db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ponto_facil.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    return app
