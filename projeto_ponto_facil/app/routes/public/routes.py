e4262dc4d0bd7cb4b11e36d84318f9d086490a15
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-08-20 21:40:04 -0300

    bronze aula 05 - pagamento

--- projeto_ponto_facil/app/routes/public/routes.py
+++ projeto_ponto_facil/app/routes/public/routes.py
@@ -5,3 +5,7 @@ bp_publico = Blueprint("public", __name__)
 @bp_publico.route("/")
 def home():
     return render_template("public/index.html")
+
+@bp_publico.route("/pagamento")
+def pagamento():
+    return render_template("public/pagamento.html")
