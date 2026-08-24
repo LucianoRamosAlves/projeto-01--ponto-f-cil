738dfdaa442f24f6a1c8a8685659e5317c88b48d
LucianoRamosAlves <141687028+LucianoRamosAlves@users.noreply.github.com>
2026-08-24 06:02:28 -0300

    iniciando rota deshboard

--- projeto_ponto_facil/app/routes/private/routes.py
+++ projeto_ponto_facil/app/routes/private/routes.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp_private = Blueprint("private", __name__)
+
+@bp_private.route("/dashboard")
+def deshboard():
+    return render_template("private/deshboard.html")
